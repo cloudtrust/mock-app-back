46b2620c66016c158d0136ce1f87bce363be0594
Christophe Frattino <christophe@frattino.ch>
2018-04-05 14:22:19 +0200

    After going through gofmt

--- cmd/mock-app-back.go
+++ cmd/mock-app-back.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
-    "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func mock(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "It works!")
+	fmt.Fprintf(w, "It works!")
 }
 
 func main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/", mock)
-    log.Fatal(http.ListenAndServe(":8000", r))
+	r := mux.NewRouter()
+	r.HandleFunc("/", mock)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
