aadb45f2931e511fa95f37af8f76774897e4ff1b
Christophe Frattino <christophe@frattino.ch>
2018-04-06 15:58:22 +0200

    Return json with hospitals

--- pkg/mockback/model.go
+++ pkg/mockback/model.go
@@ -4,35 +4,35 @@ import "time"
 
 // Hospital represents a hospital
 type Hospital struct {
-	ID          int32
-	Name        string
-	City        string
-	Departments []Department
+	ID          int32        `json:"id"`
+	Name        string       `json:"name"`
+	City        string       `json:"city"`
+	Departments []Department `json:"departments"`
 }
 
 // Department represents the department of a hospital
 type Department struct {
-	ID       int32
-	Name     string
-	Hospital Hospital
-	Doctors  []Doctor
+	ID       int32    `json:"id"`
+	Name     string   `json:"name"`
+	Hospital Hospital `json:"-"`
+	Doctors  []Doctor `json:"doctors"`
 }
 
 // Doctor represents a doctor
 type Doctor struct {
-	ID          int32
-	FirstName   string
-	LastName    string
-	Departments []Department
-	Patients    []Patient
+	ID          int32        `json:"id"`
+	FirstName   string       `json:"firstName"`
+	LastName    string       `json:"lastName"`
+	Departments []Department `json:"-"`
+	Patients    []Patient    `json:"patients"`
 }
 
 // Patient represents a patient
 type Patient struct {
-	ID        int32
-	FirstName string
-	LastName  string
-	BirthDate time.Time
-	AVSNumber string
-	Doctors   []Doctor
+	ID        int32     `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	BirthDate time.Time `json:"birthDate"`
+	AVSNumber string    `json:"avsNumber"`
+	Doctors   []Doctor  `json:"-"`
 }
--- pkg/mockback/web.go
+++ pkg/mockback/web.go
@@ -1,6 +1,8 @@
 package mockback
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,14 +10,27 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Mock returns a dummy message to confirm the web server works
+// Root returns a dummy message to confirm the web server works
 func Root(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "It works!")
+}
+
+// ListHospitals returns all the hospitals in JSON form
+func ListHospitals(w http.ResponseWriter, r *http.Request) {
+	var b bytes.Buffer
+	var enc = json.NewEncoder(&b)
+	enc.SetIndent("", "  ")
+	if err := enc.Encode(GetDummyData()); err != nil {
+		http.Error(w, err.Error(), 500)
+	} else {
+		fmt.Fprintf(w, "%s", b.String())
+	}
 }
 
 // InitWeb initializes the web service
 func InitWeb() {
 	var r = mux.NewRouter()
 	r.HandleFunc("/", Root)
+	r.HandleFunc("/hospitals", ListHospitals)
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
