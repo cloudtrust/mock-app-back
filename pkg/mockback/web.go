96a478a745e41dba14ca97a95fb805d851b3dda0
Christophe Frattino <christophe@frattino.ch>
2018-04-06 11:03:33 +0200

    Unit test to try code coverage

--- pkg/mockback/web.go
+++ pkg/mockback/web.go
@@ -8,13 +8,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func mock(w http.ResponseWriter, r *http.Request) {
+// Mock returns a dummy message to confirm the web server works
+func Mock(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "It works!")
 }
 
 // InitWeb initializes the web service
 func InitWeb() {
 	var r = mux.NewRouter()
-	r.HandleFunc("/", mock)
+	r.HandleFunc("/", Mock)
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
--- pkg/mockback/web_test.go
+++ pkg/mockback/web_test.go
@@ -1,11 +1,29 @@
 package mockback
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestAlwaysPass(t *testing.T) {
-	assert.True(t, true)
+type ResponseWriterMock struct {
+	t *testing.T
+}
+
+func (m ResponseWriterMock) Header() http.Header {
+	return nil
+}
+
+func (m *ResponseWriterMock) Write(b []byte) (int, error) {
+	assert.Equal(m.t, "It works!", string(b))
+	return 0, nil
+}
+
+func (m ResponseWriterMock) WriteHeader(statusCode int) {
+}
+
+func TestMock(t *testing.T) {
+	var m = ResponseWriterMock{t: t}
+	Mock(&m, nil)
 }
