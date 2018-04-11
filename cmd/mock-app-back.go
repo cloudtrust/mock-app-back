a6cb5114d925231595a24a01a54ff888e1f3fb09
Christophe Frattino <christophe@frattino.ch>
2018-04-11 12:38:41 +0200

    Add random number to the message

--- cmd/mock-app-back.go
+++ cmd/mock-app-back.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"time"
 
 	"github.com/cloudtrust/mock-app-back/pkg/mockback"
@@ -9,9 +11,10 @@ import (
 func main() {
 	go mockback.InitSseEndpoint()
 	go func() {
+		rand.Seed(42)
 		for {
 			time.Sleep(time.Duration(5) * time.Second)
-			mockback.SendMessage(1, "Ping!")
+			mockback.SendMessage(1, fmt.Sprintf("Ping %d!", rand.Intn(9999)))
 		}
 	}()
 
